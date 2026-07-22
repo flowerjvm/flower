cd1f4676f5b5b359150e311bf1a4f17e0e4fd92a
SBPark <pokor2@naver.com>
2026-07-23 03:38:48 +0900

    Begin Flower 0.1.2-SNAPSHOT

--- flower-check-gradle-plugin/build.gradle.kts
+++ flower-check-gradle-plugin/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "io.github.flowerjvm"
-version = providers.gradleProperty("flowerVersion").orElse("0.1.1").get()
+version = providers.gradleProperty("flowerVersion").orElse("0.1.2-SNAPSHOT").get()
 
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
